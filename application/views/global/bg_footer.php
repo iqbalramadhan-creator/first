384bb05d9306667c74381f9590d9e6ec3d1af0b1
iqbalramadhan-creator <iqbalramadhan73@gmail.com>
2019-12-18 16:54:04 +0700

    first

---
+++ application/views/global/bg_footer.php
@@ -0,0 +1,9 @@
+
+
+	<p class="footer">
+	Sistem Informasi Akademik Online UAD 2017 - ALBAL (http://ALBAL.com)<br />
+	Halaman ini dimuat selama <strong>{elapsed_time}</strong> detik
+	</p>
+</div>
+</body>
+</html>
---
+++ application/views/mahasiswa/bg_home.php
@@ -0,0 +1,26 @@
+<div id="container">
+	<h1>Beranda - Sistem Informasi Akademik Online</h1>
+
+	<div id="body">
+		<?php
+			echo $bio;
+			echo $menu;
+		?>
+		
+		<p>
+		Selamat Datang di Sistem Akademik Universitas Ahmad Dahlan Yogyakarta. 
+		Setelah melakukan login pertama kali ke Sistem Akademik ini, harap segera melakukan pergantian password pada menu Pengaturan Akun, guna menanggulangi hal-hal yang tidak diinginkan.
+		</p>
+		
+		<center><img src="../siakad-master/asset/images/logo.png" width="225" height="225" /></center>
+		
+		<div id="list">
+			<ul>
+				<li><strong>Kartu Rencana Studi</strong><br />Mengisi atau mengedit kartu rencana studi</li>
+				<li><strong>Kartu Hasil Studi</strong><br />Melihat nilai per semester yang telah ditempuh</li>
+				<li><strong>Info Kampus</strong><br />Melihat info seputar kampus dan perkuliahan</li>
+				<li><strong>Log Out</strong><br />Keluar dari sistem perwalian online</li>
+			</ul>
+		</div>
+		
+	</div>
